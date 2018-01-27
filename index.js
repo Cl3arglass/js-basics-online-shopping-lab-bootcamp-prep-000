d47242fbbed70987cb6a8968c238734e36417aa5
Holden Altaffer <haltaffer@gmail.com>
2018-01-27 03:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var cart = [];
 
+function getRandomInt() {
+    return Math.floor(Math.random() * 100) + 1
+}
+
 function getCart() {
+
  return cart;
 }
 
@@ -10,19 +15,62 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var key = item
+ cart.push({ [key]: getRandomInt() });
+ console.log(`${item} has been added to your cart.`)
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+
+  var result = ''
+
+
+    if(cart.length === 0) {
+      console.log( `Your shopping cart is empty.`)
+    } else if (getCart().length === 1) {
+      console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`)
+    } else if (getCart().length === 2) {
+      console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]} and ${Object.keys(getCart()[1])[0]} at $${getCart()[1][Object.keys(getCart()[1])[0]]}.`)
+
+    } else {
+      for (var i = 1; i < getCart().length -1; i++) {
+      result = result + ` ${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]},`
+    }
+
+console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]},` + result + ` and ${Object.keys(getCart()[getCart().length -1])[0]} at $${getCart()[getCart().length -1][Object.keys(getCart()[getCart().length -1])[0]]}.`)
+
+}
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (var i = 0; i < cart.length; i++) {
+    sum = sum + getCart()[i][Object.keys(getCart()[i])[0]]
+  }
+  return sum;
 }
 
+// The `removeFromCart()` function accepts one argument, the name of the item that should be removed.
+//     + If the cart does not contain a matching item, the function should print out `That item is not
+//     in your cart.` and return the unchanged cart.
+//     + If the item *is* present in the cart, the function should remove the object from the cart and
+//     then return the updated cart.
+//     + ***HINT***: Check each object's key (the name of the item) to see if it matches the parameter,
+//     then remove it if it matches. You might find [hasOwnProperty](https://developer.mozilla.org/en-US/do
+//       cs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty) to be useful.
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i ++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(cart.indexOf(cart[i]), 1);
+
+
+    } else {
+      console.log(`That item is not in your cart.`)
+
+    }
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
